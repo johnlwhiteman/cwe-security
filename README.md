1dabc1623dd66dc654e75967d77fdf5926377249
John L. Whiteman <>
2021-11-23 20:49:27 -0800

    Merged utils, refactored local db
    Signed-off-by: John L. Whiteman <>

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # CWE-Security
+
+CWE™ is a community-developed list of software and hardware weakness types. It serves as a common language, a measuring stick for security tools, and as a baseline for weakness identification, mitigation, and prevention efforts.
+
+This project provides an automated interface to access the latest CWEs. Teams are encouraged to map their vulnerabilities to CWEs to promote consistency.
+
+# Use Cases
+
+### Search for CWEs
+```
+from cwesecurity.cwe import Cwe
+
+cwe = Cwe()
+
+# Single input only
+print(cwe.getCwe(123))
+
+# Multiple inputs that support many types
+for c in cwe.getCwes(112, "cwe-113", ["114", 115]):
+    print(c)
+
+# No input returns all records
+for r in cwe.getCwes():
+    print(r)
+```
+
+### Search for categories
+```
+from cwesecurity.cwe import Cwe
+
+cwe = Cwe()
+
+# Single input only
+print(cwe.getCategory(123))
+
+# Multiple inputs that support many types
+for c in cwe.getCategories(112, "cwe-113", ["114", 115]):
+    print(c)
+
+# No input returns all records
+for r in cwe.getCategories():
+    print(r)
+```
+
+### Search for views
+```
+from cwesecurity.cwe import Cwe
+
+cwe = Cwe()
+
+# Single input only
+print(cwe.getView(884))
+
+# Multiple inputs that support many types
+for r in cwe.getViews(604, "660", ["1154", 1350]):
+    print(r)
+
+# No input returns all records
+for r in cwe.getViews():
+    print(r)
+```
+
+### Search for references
+```
+from cwesecurity.cwe import Cwe
+
+cwe = Cwe()
+
+# Single input only
+print(cwe.getReference("REF-14"))
+
+# Multiple inputs that support many types
+for r in cwe.getRefences("REF-23", "REF-24", ["REF-42, "REF-43"]):
+    print(r)
+
+# No input returns all records
+for r in cwe.getReferences():
+    print(r)
+```
+
+# References
+
+* [CWE Homepage](https://cwe.mitre.org/)
--- cwesecurity/cwe.py
+++ cwesecurity/cwe.py
@@ -1,14 +1,16 @@
 import glob
 import json
 import os
+import pprint
 import re
 import requests
 import sys
 import tempfile
+import types
 import xmltodict
 import zipfile
 from bs4 import BeautifulSoup
-import cweutils as cu
+from types import SimpleNamespace
 
 DOWNLOADS_URL = "https://cwe.mitre.org/data/downloads.html"
 ZIP_URL = "https://cwe.mitre.org/data/xml/cwec_latest.xml.zip"
@@ -16,18 +18,17 @@ ZIP_URL = "https://cwe.mitre.org/data/xml/cwec_latest.xml.zip"
 class Cwe():
 
     def __init__(self):
-        self.dataDir = cu.toLinuxPath(f"{os.path.dirname(__file__)}{os.sep}data")
-        self.jsonPath = cu.toLinuxPath(f"{self.dataDir}{os.sep}cwe.json")
-        self.xmlPath = cu.toLinuxPath(f"{self.dataDir}{os.sep}cwe.xml")
-        self.indexPath = cu.toLinuxPath(f"{self.dataDir}{os.sep}index.json")
-        self.catsPath = cu.toLinuxPath(f"{self.dataDir}{os.sep}cats.json")
-        self.refsPath = cu.toLinuxPath(f"{self.dataDir}{os.sep}refs.json")
-        self.viewsPath = cu.toLinuxPath(f"{self.dataDir}{os.sep}views.json")
         self.index = None
+        self.dataDir = f"{os.path.dirname(__file__)}{os.sep}data"
+        self.jsonPath = f"{self.dataDir}{os.sep}cwe.json"
+        self.xmlPath = f"{self.dataDir}{os.sep}cwe.xml"
+        self.indexPath = f"{self.dataDir}{os.sep}index.json"
+        self.cwesDir = f"{self.dataDir}{os.sep}cwes"
+        self.catsDir = f"{self.dataDir}{os.sep}categories"
+        self.refsDir = f"{self.dataDir}{os.sep}references"
+        self.viewsDir = f"{self.dataDir}{os.sep}views"
 
     def checkForUpdates(self):
-        if not self.isInstalled():
-            return True
         lVersion = self.getLocalVersion()
         if not lVersion:
             return True
@@ -36,60 +37,81 @@ class Cwe():
 
     def delete(self):
         paths = glob.glob(f"{self.dataDir}{os.sep}*.json")
-        cu.rm(paths, self.xmlPath)
+        _rm(paths, self.xmlPath)
 
-    def __download(self):
+    def _download(self):
         tmpZipPath = f"{tempfile.gettempdir()}{os.sep}cwe.zip"
-
-        # Download
         try:
             r = requests.get(ZIP_URL, stream=True)
             try:
-                cu.rm(tmpZipPath, self.jsonPath, self.xmlPath)
+                _rm(tmpZipPath, self.jsonPath, self.xmlPath)
                 with open(tmpZipPath, mode="wb") as fd:
                     fd.write(r.content)
             except (OSError, IOError) as e:
-                cu.ex("Unable to locally save the online CWE content\n{e}")
+                _ex("Unable to locally save the online CWE content\n{e}")
         except requests.exceptions.RequestException as e:
-            cu.ex("Unable to download the online CWE content:\n{e}")
-
-        # Extract
+            _ex("Unable to download the online CWE content:\n{e}")
         try:
             with zipfile.ZipFile(tmpZipPath, mode="r") as fd:
                 fd.extractall(self.dataDir)
                 tmpXmlPath = glob.glob(f"{self.dataDir}/cwec_v*.xml")[0]
                 os.rename(tmpXmlPath, self.xmlPath)
-                cu.rm(tmpZipPath)
+                _rm(tmpZipPath)
         except (IOError, zipfile.BadZipfile) as e:
-            cu.ex(f"Unable to extract downloaded CWE content:\n{e}")
-
-        # Convert/Copy XML to JSON
+            _ex(f"Unable to extract downloaded CWE content:\n{e}")
         try:
             with open(self.xmlPath, encoding="utf-8") as fd1:
                 xmlData = xmltodict.parse(fd1.read(), encoding="utf-8")
                 with open(self.jsonPath, mode="w", encoding="utf-8") as fd2:
                     json.dump(xmlData, fd2, indent=2, sort_keys=False)
         except (OSError, IOError, Exception) as e:
-            cu.ex(f"Unable to extract downloaded CWE content:\n{e}")
+            _ex(f"Unable to extract downloaded CWE content:\n{e}")
 
-    def getCwe(self, cweId, verbose=False):
+    def __get(self, group, gid, asObject=False):
         if not self.index:
             self.loadIndex()
-        cweId = re.sub("[^0-9]", "", str(cweId))
+        _gid = gid if group == "refs" else _cleanIds([gid])[0]
+        r = None
         try:
-            path = self.index[cweId]
-        except KeyError as e:
-            cu.tout(f"[Error]: Can't find CWE with Id {cweId}", verbose)
-            return None
-        return cu.read(path)
+            r = self.index.get(group, _gid)
+        except Exception as e:
+            _ex(e)
+        if asObject and r:
+            return _toObject(r)
+        return r
+
+    def __gets(self, group, gids, asObject=False):
+        if not self.index:
+            self.loadIndex()
+        try:
+            if len(gids) and gids[0]:
+                _gids = _cleanIds(gids)
+            else:
+                _gids = self.index.getIds(group)
+        except Exception as e:
+            _ex(e)
+        for _gid in _gids:
+            yield self.__get(group, _gid, asObject)
+
+    def getCategory(self, gid, asObject=False):
+        return self.__get("cats", gid, asObject)
+
+    def getCategories(self, *gids, asObject=False):
+        return self.__gets("cats", gids, asObject)
+
+    def getCwe(self, gid, asObject=False):
+        return self.__get("cwes", gid, asObject)
+
+    def getCwes(self, *gids, asObject=False):
+        return self.__gets("cwes", gids, asObject)
 
     def getLocalVersion(self):
-        if not cu.exists(self.jsonPath):
+        if not _exists(self.jsonPath):
             return None
-        content = cu.read(self.jsonPath)
+        content = _read(self.jsonPath)
         try:
             version = content["Weakness_Catalog"]["@Version"]
-            if cu.isNumber(version):
+            if _isNumber(version):
                 return float(version)
         except KeyError:
             pass
@@ -103,67 +125,202 @@ class Cwe():
             for result in results:
                 if re.search("CWE List Version", result.text, re.IGNORECASE):
                     version = result.text.split()[-1]
-                    if cu.isNumber(version):
+                    if _isNumber(version):
                         return float(version)
         return None
 
-    def isInstalled(self):
-        if not cu.exists(self.jsonPath, self.xmlPath, self.indexPath,
-                         self.catsPath, self.refsPath, self.viewsPath):
-            return False
-        return cu.exists([p for p in cu.read(self.indexPath).values()])
+    def getReference(self, gid, asObject=False):
+        return self.__get("refs", gid, asObject)
 
-    def loadCategories(self):
-        return cu.read(self.catsPath)
+    def getReferences(self, *gids, asObject=False):
+        return self.__gets("refs", gids, asObject)
+
+    def getView(self, gid, asObject=False):
+        return self.__get("views", gid, asObject)
+
+    def getViews(self, *gids, asObject=False):
+        return self.__gets("views", gids, asObject)
+
+    def isInstalled(self):
+        if not _exists(self.jsonPath, self.xmlPath, self.indexPath):
+            return False
+        results = _read(self.indexPath)
+        for key in ["cwes", "cats", "views", "refs"]:
+            if not _exists([i for i in results[key].values()]):
+                return False
+        return True
+
+    def load(self):
+        self.loadIndex()
 
     def loadIndex(self):
-        self.index = cu.read(self.indexPath)
+        self.index = _Index(self.indexPath)
+        self.index.read()
         return self.index
 
     def loadJson(self):
-        return cu.read(self.jsonPath)
-
-    def loadReferences(self):
-        return cu.read(self.refsPath)
-
-    def loadViews(self):
-        return cu.read(self.viewsPath)
-
-    def process(self):
-        if self.data:
-            self.load()
-
-    def showCwe(self, cweId):
-        cwe = self.getCwe(cweId)
-        if not cwe:
-            cu.tout(f"Can't find {cweId}")
-        cu.out(cwe)
+        return _read(self.jsonPath)
 
     def update(self):
-        def clean(c):
-            sC = json.dumps(c)
-            for t in re.findall(r'"@\w+":', sC):
-                sC = re.sub(t, t.replace("@", ""), sC)
-            return json.loads(sC)
-        self.__download()
-        index = {}
-        content = cu.read(self.jsonPath)
-        for cwe in content["Weakness_Catalog"]["Weaknesses"]["Weakness"]:
-            cwe = clean(cwe)
-            path = cu.toLinuxPath(f"{self.dataDir}{os.sep}{cwe['ID']}.json")
-            cu.write(path, cwe, sortKeys=True, verbose=False)
-            index[cwe["ID"]] = path
-        cu.write(self.indexPath, index, sortKeys=True)
-        cu.write(self.catsPath, clean(content["Weakness_Catalog"]["Categories"]["Category"]))
-        cu.write(self.refsPath, clean(content["Weakness_Catalog"]["External_References"]["External_Reference"]))
-        cu.write(self.viewsPath, clean(content["Weakness_Catalog"]["Views"]["View"]))
+        def _update(data, dataDir, index, key="ID"):
+            for d in data:
+                d = _clean(d)
+                path = f"{dataDir}{os.sep}{d[key]}.json"
+                index[d[key]] = path
+                _write(path, d, sortKeys=False)
+        self._download()
+        self.index = _Index(self.indexPath)
+        results = _read(self.jsonPath)
+        _update(results["Weakness_Catalog"]["Weaknesses"]["Weakness"],
+                self.cwesDir, self.index.cwes)
+        _update(results["Weakness_Catalog"]["Categories"]["Category"],
+                self.catsDir, self.index.cats)
+        _update(results["Weakness_Catalog"]["Views"]["View"],
+                self.viewsDir, self.index.views)
+        _update(results["Weakness_Catalog"]["External_References"]["External_Reference"],
+                self.refsDir, self.index.refs, "Reference_ID")
+        self.index.sort().save()
+
+class _Index():
+    def __init__(self, path):
+        self.path = path
+        self.cwes = {}
+        self.cats = {}
+        self.views = {}
+        self.refs = {}
+
+    def get(self, group, gid):
+        print(group, gid)
+        try:
+            path = getattr(self, group)[gid]
+            if _exists(path):
+                return _read(path)
+            else:
+                raise Exception(f"({group}) Missing group id {gid}")
+        except KeyError:
+            raise Exception(f"({group}) Unknown group id {gid}")
+
+    def getIds(self, group):
+        try:
+            return list(getattr(self, group).keys())
+        except Exception as e:
+            raise Exception(f"({group}) Unknown group")
+
+    def read(self):
+        r = _read(self.path)
+        [setattr(self, key, r[key]) \
+                 for key in ["cwes", "cats", "views", "refs"]]
+
+    def save(self):
+        _write(
+            self.path,
+            {
+                "cwes": self.cwes,
+                "cats": self.cats,
+                "views": self.views,
+                "refs": self.refs
+            },
+        )
+
+    def sort(self):
+        self.cwes = {k:self.cwes[k] for k in sorted(self.cwes, key=int)}
+        self.cats = {k:self.cats[k] for k in sorted(self.cats, key=int)}
+        self.views = {k:self.views[k] for k in sorted(self.views, key=int)}
+        self.refs = {k:self.refs[k] for k in sorted(self.refs, key=str)}
+        return self
+
+def _clean(c):
+    sC = json.dumps(c)
+    for t in re.findall(r'"@\w+":', sC):
+        sC = re.sub(t, t.replace("@", ""), sC)
+    return json.loads(sC)
+
+def _cleanIds(ids):
+    _ids = list(map(lambda i: re.sub("[^0-9]", "",
+                str(i)), _toList(ids)))
+    return _ids
+
+def _ex(msg, dontExit=False):
+    print(f"[Exception]: {msg}")
+    if not dontExit:
+        sys.exit(1)
+
+def _exists(*paths, exit=False):
+    _paths = _toList(paths)
+    notExists = False
+    for _path in _paths:
+        if not os.path.exists(_path):
+            if exit:
+                sys.exit(1)
+            notExists = True
+            break
+    return not notExists
+
+def _isNumber(token):
+    try:
+        float(token)
+    except ValueError:
+        return False
+    return True
+
+def _out(msg, asText=False, sort=False, indent=1):
+    if asText:
+        print(msg)
+    else:
+        pprint.pprint(msg, sort_dicts=sort, indent=indent)
+
+def _read(*paths):
+    results = [r for r in _readG(_toList(paths))]
+    if len(results) < 2:
+        return results[0]
+    return results
+
+def _readG(*paths):
+    _paths = _toList(paths)
+    try:
+        for _path in _paths:
+            with open(_path) as fd:
+                yield json.load(fd)
+    except (OSError, IOError) as e:
+        _ex("Can't read {_paths}: {e}")
+
+def _rm(*paths):
+    _paths = _toList(paths)
+    try:
+        [os.remove(_path) for _path in _paths]
+    except OSError:
+        pass
+
+def _toList(args):
+    _args = []
+    for _ in list(args):
+        if isinstance(_, list):
+            _args.extend(_)
+        elif isinstance(_, set):
+            _args.extend(_, list(set))
+        else:
+            _args.append(_)
+    return _args
+
+def _toObject(data):
+    _data = data
+    if isinstance(_data, dict):
+        _data = json.dumps(_data)
+    return json.loads(_data, object_hook=lambda d: SimpleNamespace(**d))
+
+def _write(path, content, sortKeys=False):
+    try:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, "w", encoding="utf-8") as fd:
+            json.dump(content, fd, indent=2, sort_keys=sortKeys)
+    except (OSError, IOError) as e:
+        _ex(f"Can't write to {path}: {e}")
 
 if __name__ == "__main__":
-    c = Cwe()
-    #c.delete()
-    if c.checkForUpdates():
-        print("Retrieving latest online CWE content")
-        c.update()
-    cwe = c.getCwe(1312)
-    cu.out(cwe)
-    sys.exit()
+    cwe = Cwe()
+    x = cwe.getReferences()
+    i = 0
+    for _x in x:
+        print(_x)
+        i += 1
+    print(i)
